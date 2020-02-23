85a588aaa52934d3a6ac83bca3ed139ee5ce5e9f
kwaku <44327202+mr-nansi@users.noreply.github.com>
2020-02-23 20:36:13 +0000

    working query app

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cookbook.ingredients',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+GRAPHENE = {
+    'SCHEMA': 'cookbook.schema.schema'
+}
